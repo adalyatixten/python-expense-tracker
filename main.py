c58610457821885134cdaa1587d0a92f6a4334bf
Adalyat Orduhani <adalyatixten@gmail.com>
2026-09-10 00:27:22 +0800

    Add basic expense input

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Expense Tracker")
+print("---------------")
+
+amount = float(input("Enter expense amount: "))
+category = input("Enter category: ")
+
+print()
+print("Expense added")
+print(f"Amount: RM {amount:.2f}")
+print(f"Category: {category}")
