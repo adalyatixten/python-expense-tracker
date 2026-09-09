bb68bdfb148ae2c58605eaaeb7df40f73e1eda15
Adalyat Orduhani <adalyatixten@gmail.com>
2026-09-10 00:34:13 +0800

    Add CSV expense storage

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import csv
+
 print("Expense Tracker")
 print("---------------")
 
 amount = float(input("Enter expense amount: "))
 category = input("Enter category: ")
+
+with open("expenses.csv", "a", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow([amount, category])
 
 print()
 print("Expense added")
